735ca14869be7254ff676552a29ade037182d560
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-02-01 00:37:06 +0530

    Create insuremonkey.user.js

---
+++ insuremonkey.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         InsureMonkey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://ipartner.icicilombard.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        unsafeWindow
+// @run-at document-end
+// ==/UserScript==
+
+
+unsafeWindow.console.log("hi from monkey");
+
+const helperFunction = (e)=>{
+    e.stopPropagation()
+}
+
+const removeKeyRestrictions = (events)=>{
+    events.forEach(event=>{
+        unsafeWindow.addEventListener(event,helperFunction,{capture:true})
+
+    })
+}
+
+removeKeyRestrictions(["keypress","keydown"])
